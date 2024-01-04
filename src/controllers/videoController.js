5acaf822bded6dffe7b50d6653634649a4947df9
vmfkdla159 <vmfkdla159@gmail.com>
2024-01-04 11:44:30 +0900

    2024.01.04

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,15 @@
+const fakeUser = {
+    username:"midwinter",
+    loggedIn: false,
+};
+
+
 //globalRouter
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
 export const search = (req, res) => res.send("Search");
 
 //videoRouter
-export const watch = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit Video");
+export const watch = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
