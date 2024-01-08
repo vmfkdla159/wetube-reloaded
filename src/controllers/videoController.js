4250f10c3349a82bcf0a73d5dd2c2606ea75d888
vmfkdla159 <vmfkdla159@gmail.com>
2024-01-08 11:11:04 +0900

    2024.01.08_Error
    somthings wrong..
    but I don't know

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -67,6 +67,26 @@ export const getEdit = (req, res) => {
     const video = videos[id-1];
     return res.render("edit", {pageTitle:`Editing:${video.title}`, video})
 };
-export const postEdit = (req, res) => {}; 
-export const deleteVideo = (req, res) => res.send("Delete Video");
-export const upload = (req, res) => res.send("Upload Video");
+export const postEdit = (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+}; 
+
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle:`Upload Video`});
+}
+
+export const postUpload = (req, res) => {
+    const newVideo = {
+        title: req.body.title,
+        rating: 0,
+        comment: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,10 @@
 import express, { Router } from "express";
-import { watch, getEdit, deleteVideo, upload, postEdit } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
